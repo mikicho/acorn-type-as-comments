a2ef2df96bc830555290c701baa32655baa8ce5e
Michael Solomon <micheal540@gmail.com>
2022-07-24 01:26:36 +0300

    improve tests

--- tests/func-arg-types.test.js
+++ tests/func-arg-types.test.js
@@ -1,4 +1,5 @@
 import * as acorn from "acorn";
+import { generate } from "escodegen";
 import tacParser from "../src/tac-parser.js";
 
 const parser = acorn.Parser.extend(tacParser())
@@ -9,17 +10,17 @@ const options = {
 
 describe('Function argument type', () => {
   it.each([
-    ['function a(arg) {}', {"type":"Program","start":0,"end":18,"body":[{"type":"FunctionDeclaration","start":0,"end":18,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"Identifier","start":11,"end":14,"name":"arg"}],"body":{"type":"BlockStatement","start":16,"end":18,"body":[]}}],"sourceType":"module"}],
-    ['function a(arg = 5) {}', {"type":"Program","start":0,"end":22,"body":[{"type":"FunctionDeclaration","start":0,"end":22,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"AssignmentPattern","start":11,"end":18,"left":{"type":"Identifier","start":11,"end":14,"name":"arg"},"right":{"type":"Literal","start":17,"end":18,"value":5,"raw":"5"}}],"body":{"type":"BlockStatement","start":20,"end":22,"body":[]}}],"sourceType":"module"}],
-    ['function a(arg: string) {}', {"type":"Program","start":0,"end":26,"body":[{"type":"FunctionDeclaration","start":0,"end":26,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"Identifier","start":11,"end":14,"name":"arg"}],"body":{"type":"BlockStatement","start":24,"end":26,"body":[]}}],"sourceType":"module"}],
-    ['function a(arg: Pick<A, "a">) {}', {"type":"Program","start":0,"end":32,"body":[{"type":"FunctionDeclaration","start":0,"end":32,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"Identifier","start":11,"end":14,"name":"arg"}],"body":{"type":"BlockStatement","start":30,"end":32,"body":[]}}],"sourceType":"module"}],
-    ['function a(arg: Pick<A, "a">, arg1) {}', {"type":"Program","start":0,"end":38,"body":[{"type":"FunctionDeclaration","start":0,"end":38,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"Identifier","start":11,"end":14,"name":"arg"},{"type":"Identifier","start":30,"end":34,"name":"arg1"}],"body":{"type":"BlockStatement","start":36,"end":38,"body":[]}}],"sourceType":"module"}],
-    ['function a(arg: Pick<A, "a"> = 5) {}', {"type":"Program","start":0,"end":36,"body":[{"type":"FunctionDeclaration","start":0,"end":36,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"AssignmentPattern","start":11,"end":32,"left":{"type":"Identifier","start":11,"end":14,"name":"arg"},"right":{"type":"Literal","start":31,"end":32,"value":5,"raw":"5"}}],"body":{"type":"BlockStatement","start":34,"end":36,"body":[]}}],"sourceType":"module"}],
-    ['function a(arg: Pick(A, "a") = 5) {}', {"type":"Program","start":0,"end":36,"body":[{"type":"FunctionDeclaration","start":0,"end":36,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"AssignmentPattern","start":11,"end":32,"left":{"type":"Identifier","start":11,"end":14,"name":"arg"},"right":{"type":"Literal","start":31,"end":32,"value":5,"raw":"5"}}],"body":{"type":"BlockStatement","start":34,"end":36,"body":[]}}],"sourceType":"module"}],
-    ['function a(arg: Pick[A, "a"] = 5) {}', {"type":"Program","start":0,"end":36,"body":[{"type":"FunctionDeclaration","start":0,"end":36,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"AssignmentPattern","start":11,"end":32,"left":{"type":"Identifier","start":11,"end":14,"name":"arg"},"right":{"type":"Literal","start":31,"end":32,"value":5,"raw":"5"}}],"body":{"type":"BlockStatement","start":34,"end":36,"body":[]}}],"sourceType":"module"}],
-    ['function a(arg: string = 5, arg1) {}', {"type":"Program","start":0,"end":36,"body":[{"type":"FunctionDeclaration","start":0,"end":36,"id":{"type":"Identifier","start":9,"end":10,"name":"a"},"expression":false,"generator":false,"async":false,"params":[{"type":"AssignmentPattern","start":11,"end":26,"left":{"type":"Identifier","start":11,"end":14,"name":"arg"},"right":{"type":"Literal","start":25,"end":26,"value":5,"raw":"5"}},{"type":"Identifier","start":28,"end":32,"name":"arg1"}],"body":{"type":"BlockStatement","start":34,"end":36,"body":[]}}],"sourceType":"module"}],
-  ])('should parse: %s', (source, expectedAst) => {
+    ['function a(arg) {}', 'function a(arg) {\n}'],
+    ['function a(arg = 5) {}', 'function a(arg = 5) {\n}'],
+    ['function a(arg: string) {}', 'function a(arg) {\n}'],
+    ['function a(arg: Pick<A, "a">) {}', 'function a(arg) {\n}'],
+    ['function a(arg: Pick<A, "a">, arg1) {}', 'function a(arg, arg1) {\n}'],
+    ['function a(arg: Pick<A, "a"> = 5) {}', 'function a(arg = 5) {\n}'],
+    ['function a(arg: Pick(A, "a") = 5) {}', 'function a(arg = 5) {\n}'],
+    ['function a(arg: Pick[A, "a"] = 5) {}', 'function a(arg = 5) {\n}'],
+    ['function a(arg: string = 5, arg1) {}', 'function a(arg = 5, arg1) {\n}'],
+  ])('should parse: %s', (source, expected) => {
     const ast = parser.parse(source, options)
-    expect(ast).toEqual(expectedAst)
+    expect(generate(ast)).toBe(expected)
   });
 });
--- tests/variable-types.test.js
+++ tests/variable-types.test.js
@@ -1,4 +1,5 @@
 import * as acorn from "acorn";
+import { generate } from "escodegen";
 import tacParser from "../src/tac-parser.js";
 
 const parser = acorn.Parser.extend(tacParser())
@@ -9,12 +10,12 @@ const options = {
 
 describe('Variable type', () => {
   it.each([
-    ['let coord', {"type":"Program","start":0,"end":9,"body":[{"type":"VariableDeclaration","start":0,"end":9,"declarations":[{"type":"VariableDeclarator","start":4,"end":9,"id":{"type":"Identifier","start":4,"end":9,"name":"coord"},"init":null}],"kind":"let"}],"sourceType":"module"}],
-    ['const coord = 5', {"type":"Program","start":0,"end":15,"body":[{"type":"VariableDeclaration","start":0,"end":15,"declarations":[{"type":"VariableDeclarator","start":6,"end":15,"id":{"type":"Identifier","start":6,"end":11,"name":"coord"},"init":{"type":"Literal","start":14,"end":15,"value":5,"raw":"5"}}],"kind":"const"}],"sourceType":"module"}],
-    ['let coord: Pair<number>', {"type":"Program","start":0,"end":23,"body":[{"type":"VariableDeclaration","start":0,"end":15,"declarations":[{"type":"VariableDeclarator","start":4,"end":15,"id":{"type":"Identifier","start":4,"end":9,"name":"coord"},"init":null}],"kind":"let"}],"sourceType":"module"}],
-    ['const coord: Pair<number> = 5', {"type":"Program","start":0,"end":29,"body":[{"type":"VariableDeclaration","start":0,"end":29,"declarations":[{"type":"VariableDeclarator","start":6,"end":29,"id":{"type":"Identifier","start":6,"end":11,"name":"coord"},"init":{"type":"Literal","start":28,"end":29,"value":5,"raw":"5"}}],"kind":"const"}],"sourceType":"module"}],
-  ])('should parse: %s', (source, expectedAst) => {
+    ['let coord', 'let coord;'],
+    ['const coord = 5', 'const coord = 5;'],
+    ['let coord: Pair<number>', 'let coord;'],
+    ['const coord: Pair<number> = 5', 'const coord = 5;'],
+  ])('should parse: %s', (source, expected) => {
     const ast = parser.parse(source, options)
-    expect(ast).toEqual(expectedAst)
+    expect(generate(ast)).toBe(expected)
   });
 });
