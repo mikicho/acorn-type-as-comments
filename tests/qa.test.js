c19e63950540ef12fd7530a507d7a70af7cbb6ea
Gil Tayar <gil@tayar.org>
2022-09-04 19:07:35 +0300

    add tests for unsupported syntax and bugs (#1)

---
+++ tests/qa.test.js
@@ -0,0 +1,37 @@
+import * as acorn from "acorn";
+import {toJs} from 'estree-util-to-js'
+import tacParser from "../src/tac-parser.js";
+
+const parser = acorn.Parser.extend(tacParser())
+const options = {
+  ecmaVersion: 'latest',
+  sourceType: "module",
+}
+
+describe.skip('qa', () => {
+  it('should parse lotsa crazy code', () => {
+    const source = `
+let x: string
+let y: (whatever is necessary);
+let z: whatever is necessary = 4
+
+function f(x: this<is-[really]-{good}>, y: (string extends State
+  ? ParserError<"EatWhitespace got generic string type">
+  : State extends \` \${x}\` | \`\${infer State}\`
+    ? EatWhitespace<State>
+    : State))  {}
+interface A {
+  a: !!!^^^hello!
+}
+type B = {a: string!hello?&&&, [[]]}
+class C {
+  a: string;
+  constructor(a: <>) {}
+  foo(a: !!![]) {}
+};
+function optional(x?: string) {}
+    `.trim()
+    const ast = parser.parse(source, options)
+    expect(toJs(ast).value).toBe('')
+  });
+});
--- tests/type-alias.test.js
+++ tests/type-alias.test.js
@@ -12,8 +12,14 @@ describe('type alias', () => {
   it.each([
     ['type = 5', 'type = 5;'],
     ['a = 1; type MyString = string', 'a = 1;'],
+    // ['a = 1;\ntype MyString = string\nb=1', 'a = 1;\nb = 1;'], // TODO: The problem is ASI in the first line
     ['type Pair<T, U> = { l: T r: U }', ''],
     ['type Pair<T, U> = { l: T, r: U }; a = 1', 'a = 1;'],
+    ['type Pair<T, U> = { l: T, r: U } a = 1', 'a = 1;'],
+    ['export type Pair<T, U> = { l: T, r: U } a = 1', 'a = 1;'],
+    ['export type Pair<T!! or die or [[(())]], U?!?!?!?!? right?> = { l: T, r: U } a = 1', 'a = 1;'],
+    ['export type { Pair }\na = 1', 'a = 1;'],
+    ['export type { Pair as Triplet}\na = 1', 'a = 1;'],
   ])('should parse: %s', (source, expected) => {
     const ast = parser.parse(source, options)
     expect(generate(ast)).toBe(expected)
