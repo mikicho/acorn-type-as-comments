dc32888a1732706f2ff2975e8fb67af3877a45e5
Gil Tayar <gil@tayar.org>
2022-09-09 12:34:55 +0300

    add prettier

--- src/tac-parser.js
+++ src/tac-parser.js
@@ -1,28 +1,28 @@
-import { Parser, TokContext, isIdentifierStart } from "acorn";
+import {Parser, TokContext, isIdentifierStart} from 'acorn'
 
 export default function (options = {}) {
-  return plugin;
+  return plugin
 }
 
 /**
  * @param {Parser} parser
  */
 function plugin(parser) {
-  const tt = parser.acorn.tokTypes;
+  const tt = parser.acorn.tokTypes
   const contexts = Object.assign(parser.acorn.tokContexts, {
-    a_stat: new TokContext("<", false),
-    s_stat: new TokContext("[", false),
+    a_stat: new TokContext('<', false),
+    s_stat: new TokContext('[', false),
   })
   const keywordsTypes = parser.acorn.keywordTypes
 
   // TODO: maybe we can duplicate parseIndent instead of both parseMaybeDefault and parseVar
   return class extends Parser {
     readWord() {
-      const word = this.readWord1();
+      const word = this.readWord1()
 
       if (word === 'export') {
         this.skipSpace()
-        const nextWord = this.peekWord();
+        const nextWord = this.peekWord()
         if (['type', 'interface'].includes(nextWord)) {
           this.skipTypeAlias()
           return
@@ -35,7 +35,10 @@ function plugin(parser) {
           this.next()
           return
         }
-      } else if (['type', 'interface'].includes(word) && this.skipSpace(), isIdentifierStart(this.fullCharCodeAtPos())) {
+      } else if (
+        (['type', 'interface'].includes(word) && this.skipSpace(),
+        isIdentifierStart(this.fullCharCodeAtPos()))
+      ) {
         this.skipTypeAlias()
         return
       }
@@ -45,38 +48,44 @@ function plugin(parser) {
         type = keywordsTypes[word]
       }
       return this.finishToken(type, word)
-   }
+    }
 
     parseMaybeDefault(startPos, startLoc, left) {
-      left = left || this.parseBindingAtom();
+      left = left || this.parseBindingAtom()
       if (this.type === tt.colon) {
         this.skipParameterType()
       }
-      if (this.options.ecmaVersion < 6 || !this.eat(tt.eq)) { return left }
-      var node = this.startNodeAt(startPos, startLoc);
-      node.left = left;
-      node.right = this.parseMaybeAssign();
-      return this.finishNode(node, "AssignmentPattern")
+      if (this.options.ecmaVersion < 6 || !this.eat(tt.eq)) {
+        return left
+      }
+      var node = this.startNodeAt(startPos, startLoc)
+      node.left = left
+      node.right = this.parseMaybeAssign()
+      return this.finishNode(node, 'AssignmentPattern')
     }
 
     skipParameterType() {
       let code = this.input.charCodeAt(this.pos)
       const contextsCount = this.context.length
 
-      loop: while ((![ascii(')'), ascii(','), ascii(';'), ascii('='), ascii('>'), ascii('}')].includes(code) || contextsCount < this.context.length) && this.pos < this.input.length) {
+      loop: while (
+        (![ascii(')'), ascii(','), ascii(';'), ascii('='), ascii('>'), ascii('}')].includes(code) ||
+          contextsCount < this.context.length) &&
+        this.pos < this.input.length
+      ) {
         switch (code) {
           case ascii('<'):
             this.context.push(contexts.a_stat)
-            break;
+            break
           case ascii('('):
             this.context.push(contexts.p_stat)
-            break;
+            break
           case ascii('['):
             this.context.push(contexts.s_stat)
-            break;
+            break
           case ascii('{'):
             this.context.push(contexts.b_stat)
-            break;
+            break
           case ascii('>'):
           case ascii(')'):
           case ascii(']'):
@@ -87,7 +96,7 @@ function plugin(parser) {
               this.pos++
               break loop
             }
-            break;
+            break
         }
         code = this.input.charCodeAt(++this.pos)
       }
@@ -102,7 +111,7 @@ function plugin(parser) {
     parseVar(node, isFor, kind) {
       node.declarations = []
       node.kind = kind
-      for (; ;) {
+      for (;;) {
         let decl = this.startNode()
         this.parseVarId(decl, kind)
         if (this.type === tt.colon) {
@@ -110,21 +119,27 @@ function plugin(parser) {
         }
         if (this.eat(tt.eq)) {
           decl.init = this.parseMaybeAssign(isFor)
-        } else if (kind === "const" && !(this.type === tt._in || (this.options.ecmaVersion >= 6 && this.isContextual("of")))) {
+        } else if (
+          kind === 'const' &&
+          !(this.type === tt._in || (this.options.ecmaVersion >= 6 && this.isContextual('of')))
+        ) {
           this.unexpected()
-        } else if (decl.id.type !== "Identifier" && !(isFor && (this.type === tt._in || this.isContextual("of")))) {
-          this.raise(this.lastTokEnd, "Complex binding patterns require an initialization value")
+        } else if (
+          decl.id.type !== 'Identifier' &&
+          !(isFor && (this.type === tt._in || this.isContextual('of')))
+        ) {
+          this.raise(this.lastTokEnd, 'Complex binding patterns require an initialization value')
         } else {
           decl.init = null
         }
-        node.declarations.push(this.finishNode(decl, "VariableDeclarator"))
+        node.declarations.push(this.finishNode(decl, 'VariableDeclarator'))
         if (!this.eat(tt.comma)) break
       }
       return node
     }
 
     skipTypeAlias() {
-      this.pos = this.input.indexOf("{", this.pos) + 1
+      this.pos = this.input.indexOf('{', this.pos) + 1
       let code = this.input.charCodeAt(this.pos)
       const contextsCount = this.context.length
       this.context.push(contexts.b_stat)
@@ -132,24 +147,24 @@ function plugin(parser) {
         switch (code) {
           case ascii('<'):
             this.context.push(contexts.a_stat)
-            break;
+            break
           case ascii('('):
             this.context.push(contexts.p_stat)
-            break;
+            break
           case ascii('['):
             this.context.push(contexts.s_stat)
-            break;
+            break
           case ascii('{'):
-            this. context.push(contexts.b_stat)
-            break;
+            this.context.push(contexts.b_stat)
+            break
           case ascii('>'):
           case ascii(')'):
           case ascii(']'):
           case ascii('}'):
             this.context.pop()
-            break;
+            break
         }
-        code = this.input.charCodeAt(++this.pos);
+        code = this.input.charCodeAt(++this.pos)
       }
 
       if (code === ascii(';')) {
@@ -164,7 +179,6 @@ function plugin(parser) {
       if (this.eat(tt.relational)) {
         this.parseClassGeneric()
       }
-
     }
 
     parseClassGeneric() {
@@ -177,9 +191,9 @@ function plugin(parser) {
             this.context.push(contexts.a_stat)
           case 62: // >
             this.context.pop()
-            break;
+            break
         }
-        code = this.input.charCodeAt(++this.pos);
+        code = this.input.charCodeAt(++this.pos)
       }
       this.next()
     }
@@ -194,13 +208,13 @@ function plugin(parser) {
     parseFunctionParams(node) {
       super.parseFunctionParams(node)
 
-      if(this.type === tt.colon) {
-          this.skipParameterType()
+      if (this.type === tt.colon) {
+        this.skipParameterType()
       }
     }
 
     peekWord() {
-      return this.input.slice(this.pos, this.input.indexOf(" ", this.pos))
+      return this.input.slice(this.pos, this.input.indexOf(' ', this.pos))
     }
   }
 }
