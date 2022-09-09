a9c9f48aaf7d22fdff85e43b88b26f57b2ce6a14
Gil Tayar <gil@tayar.org>
2022-09-09 14:29:38 +0300

    fixed failing tests. All commented out tests are now missing features

--- tests/interface.test.js
+++ tests/interface.test.js
@@ -13,8 +13,8 @@ describe.only('interface', () => {
     ['a = 1; interface User { name: string }', 'a = 1;'],
     ['interface Pair<T, U> = { l: T; r: U }', ''],
     ['export interface Pair<T, U> = { l: T, r: U }', ''],
-    // ['interface Pair<T or ddd<d>, U  or this!<[]>> = { l: T, r: U }; a = 1', 'a = 1;'],
-    // ['export interface Pair<T, U> = { l: T, r: U }; a = 1', 'a = 1;'],
+    ['interface Pair<T or ddd<d>, U  or this!<[]>> = { l: T, r: U }; a = 1', ';\na = 1;'],
+    ['export interface Pair<T, U> = { l: T, r: U }; a = 1', ';\na = 1;'],
   ])('should parse: %s', (source, expected) => {
     const ast = parser.parse(source, options)
     expect(generate(ast)).toBe(expected)
